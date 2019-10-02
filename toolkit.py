4522934c8c16c76e3129495c9e7b04fd71518b6d
Eug-VS <eug-vs@keemail.me>
2019-10-03 01:36:23 +0300

    Implement Gaussian elimination algorithm
    -Solve SLAE
    -Compute determinant of the matrix
    -Find the inverse matrix
    
    All the actions are performed on the SLAE.system which is a
    numpy array of size (N x N*2+1), which respectively consists of:
    -Original matrix (N x N)
    -Vector of solutions (N x 1)
    -Inverse matrix (N x N)

--- toolkit.py
+++ toolkit.py
@@ -3,9 +3,56 @@ import numpy as np
 
 class SLAE:
     def __init__(self, matrix, vector):
-        self.matrix = np.array(matrix)
-        self.vector = np.array(vector).reshape(len(vector), 1)
+        v = np.array(vector)[np.newaxis].T
+        self.system = np.append(matrix, v, axis=1)
+        self.dimension = len(vector)
+        self.determinant = 1
+
+        reversed = np.zeros((self.dimension, self.dimension))
+        for i in range(self.dimension):
+            reversed[i][i] = 1
+
+        self.system = np.append(self.system, reversed, axis=1)
 
     def __str__(self):
-        nonhomogeneous = np.append(self.matrix, self.vector, axis=1)
-        return str(nonhomogeneous)
+        return np.array2string(self.system, max_line_width=np.inf)
+
+    def forward(self, index):
+        terminator = self.system[index]
+        divider = terminator[index]
+
+        for i in range(index, self.dimension):
+            row = self.system[i]
+            if i != index:
+                target = row[index]
+                row = [item - t * target/divider for item, t in zip(row, terminator)]
+                self.system[i] = row
+
+    def reverse(self, index):
+        terminator = self.system[index]
+        leader = terminator[index]
+        terminator = [item / leader for item in terminator]
+        self.system[index] = terminator
+
+        for i in range(index):
+            row = self.system[i]
+            target = row[index]
+            row = [item - t * target for item, t in zip(row, terminator)]
+            self.system[i] = row
+
+    def get_matrix(self):
+        return self.system[:, :self.dimension]
+
+    def get_solutions(self):
+        return self.system[:, self.dimension]
+
+    def get_inverse_matrix(self):
+        return self.system[:, -self.dimension:]
+
+    def gaussian_elimination(self):
+        for i in range(self.dimension):
+            self.forward(i)
+            self.determinant *= self.system[i][i]
+
+        for i in reversed(range(self.dimension)):
+            self.reverse(i)
