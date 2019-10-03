ad42dcb4b595e323b394f467c5dd2bbeb9d3a745
Eug-VS <eug-vs@keemail.me>
2019-10-03 08:09:27 +0300

    Significantly simplify code, remove redundant comments
    -Unleash the power of NumPy
    -Rename multiply_back() -> verification_matrix()
    -Define alpha = k/2 according to main task

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from toolkit import *
 
+
 k = -3
-k /= 2
+alpha = k / 2
 
-A = [[3.81, 0.25, 1.28, 0.75 + k],
-     [2.25, 1.32, 4.58 + k, 0.49],
-     [5.31, 6.28 + k, 0.98, 1.04],
-     [9.39 + k, 2.45, 3.35, 2.28]]
+A = [[3.81, 0.25, 1.28, 0.75 + alpha],
+     [2.25, 1.32, 4.58 + alpha, 0.49],
+     [5.31, 6.28 + alpha, 0.98, 1.04],
+     [9.39 + alpha, 2.45, 3.35, 2.28]]
 
-f = [4.21, 6.47 + k, 2.38, 10.48 + k]
+f = [4.21, 6.47 + alpha, 2.38, 10.48 + alpha]
 
 S = SLAE(A, f)
 print('Initial matrix: ')
@@ -20,6 +21,6 @@ print('\nDeterminant:', S.determinant)
 print('\nInverse matrix: ')
 print(S.extract_inverse_matrix())
 print('\nResidual vector: ')
-print(S.get_residual())
+print(S.residual())
 print('\nInverse matrix * original matrix: ')
-print(S.multiply_back())
+print(S.verification_matrix())
