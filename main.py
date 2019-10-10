633fb3eedb289185288a7acdd7cc47fe95c8801e
Eug-VS <eug-vs@keemail.me>
2019-10-10 11:06:17 +0300

    Rework algorithm to exactly match Gaussian one
    Previous version was reffered to as Gauss-Jordan elimination.

--- main.py
+++ main.py
@@ -8,18 +8,17 @@ A = [[3.81, 0.25, 1.28, 0.75 + alpha],
      [2.25, 1.32, 4.58 + alpha, 0.49],
      [5.31, 6.28 + alpha, 0.98, 1.04],
      [9.39 + alpha, 2.45, 3.35, 2.28]]
-
 f = [4.21, 6.47 + alpha, 2.38, 10.48 + alpha]
 
 S = SLAE(A, f)
 print('Initial matrix: ')
-print(S.extract_matrix())
+print(S.initial_matrix)
 S.gaussian_elimination()
 print('\nVector of solutions: ')
-print(S.extract_solutions())
+print(S.solution)
 print('\nDeterminant:', S.determinant)
 print('\nInverse matrix: ')
-print(S.extract_inverse_matrix())
+print(S.inverse)
 print('\nResidual vector: ')
 print(S.residual())
 print('\nInverse matrix * original matrix: ')
