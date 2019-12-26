79c81fbc32651c744195690b5cc4977927f511f7
Eug-VS <eug-vs@keemail.me>
2019-12-26 13:37:07 +0300

    Fix incorrect matrix

--- main.py
+++ main.py
@@ -11,16 +11,15 @@ if __name__ == '__main__':
               [9.39 + alpha, 2.45, 3.35, 2.28]]
     f = [4.21, 6.47 + alpha, 2.38, 10.48 + alpha]
 
-    print(f'Initial matrix:')
-    Matrix.print(matrix, f)
-    t_initial = [[7.6515, 2.2375, 2.262, 0.3675],
-                 [1.50, 7.53, -0.30, -1.21],
-                 [2.25, 1.32, 6.08, 0.49],
-                 [3.81, 0.25, 1.28, 2.25]]
-    t_free_terms = [8.4015, -1.83, 7.97, 4.21]
+    diag_dominant = [[3.81, 0.25, 1.28, -0.75],
+                     [-0.33, 3.65, 0.71, -0.75],
+                     [0.81, 2.14, -5.18, 0.06],
+                     [0.27, 1.95, 0.79, 3.78]]
+    new_f = [4.21, -1.63, -7.56, 0.56]
+    iterative_methods = SLAE(diag_dominant, new_f, 10 ** 5)
 
-    iterative_methods = SLAE(t_initial, t_free_terms, 10**5)
-    print()
+    print(f'Matrix:\n{iterative_methods.initial_matrix}')
+    print(f'Vector: {iterative_methods.vector}')
     t1 = time.perf_counter()
     x1, n1, B, b = iterative_methods.simple_iterative_method()
     t2 = time.perf_counter()
@@ -33,12 +32,11 @@ if __name__ == '__main__':
     x2, n2 = iterative_methods.jacobi_method()
     t4 = time.perf_counter()
     print(f'Jacobi method:\nTransformed matrix: ')
-    Matrix.print(t_initial, t_free_terms)
+    Matrix.print(diag_dominant, new_f)
     print(f'B:')
     Matrix.print(B)
     print(f'b:{b}')
     print(f'||B||: {Matrix.norm(B)}')
-    print(f'||b||: {Matrix.norm_for_vector(b)}')
     print(f'x vector = {x2}\niterations number: {n2}')
     print(f'incoherence: {iterative_methods.incoherence(x2)}')
     print(f'Time: { t4 - t3}\n')
